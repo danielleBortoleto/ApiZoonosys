fd5cc110f3ccc0443c560c48fbcf4888973fca63
daniellebortoleto <danielle.bortoleto3412@gmail.com>
2025-08-27 11:12:04 -0300

    Implemented UserDetails components in User.java

--- src/main/java/com/zoonosys/demo/models/User.java
+++ src/main/java/com/zoonosys/demo/models/User.java
@@ -1,32 +1,80 @@
 package com.zoonosys.demo.models;
 
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 
 @Entity
 @Table(name = "users")
-@Getter
-@Setter
-@NoArgsConstructor
-public class User {
+public class User implements UserDetails{   
+    //Componente UserDetails do Spring Security
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy=GenerationType.SEQUENCE)
     private Long id;
+    @Column(nullable = false, unique = true)
     private String email;
+    @Column(nullable = false)
     private String password;
 
-    public User(Long id) {
-        this.id = id;
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updateAt;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities(){
+        return List.of(new SimpleGrantedAuthority("ROLE_USER"));
     }
 
-    public User(Long id, String password, String email) {
-        this.id = id;
-        this.password = password;
-        this.email = email;
+    @Override
+    public String getUsername(){
+        return this.email;
+    }
+
+    @Override
+    public String getPassword(){
+        return this.password;
+    }
+
+    //Por enquanto retorna "true" para indicar a conta sempre ativa.
+    @Override
+    public boolean isAccountNonExpired(){
+        return true;
+    }
+
+    @Override 
+    public boolean isAccountNonLocked(){
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() { return true; }
+
+    @Override
+    public boolean isEnabled(){
+        return true;
     }
 }
--- src/main/java/com/zoonosys/demo/repositories/UserRepository.java
+++ src/main/java/com/zoonosys/demo/repositories/UserRepository.java
@@ -4,6 +4,8 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.zoonosys.demo.models.User;
+
 public interface UserRepository extends JpaRepository<User, Long>{
     Optional<User> findByUsername(String email);
 }
